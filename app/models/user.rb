8e82e9d11cfaac9d6df510b4681bfa4cb268aca2
Gen Tamura <gen.tamura84@gmail.com>
2021-07-12 13:40:00 +0900

    Add avatar in user by active storage

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :avatar
+
+  validate :avatar_content_type, if: :avatar_attached?
+
+  def avatar_content_type
+    extension = ['image/png', 'image/jpg', 'image/jpeg', 'image/gif']
+    return if avatar.content_type.in?(extension)
+
+    ext_str = extension.map { |t| t.sub('image/', '') }.join('、')
+    errors.add(User.human_attribute_name(:avatar), "の拡張子が正しくありません。#{ext_str}のいずれかの画像をアップロードしてください。")
+  end
+
+  private
+
+  def avatar_attached?
+    avatar.attached?
+  end
 end
