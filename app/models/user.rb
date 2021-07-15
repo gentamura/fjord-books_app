ff5de1d27ff0838758a3efec7421698a73c5f2bd
Gen Tamura <gen.tamura84@gmail.com>
2021-07-15 17:29:21 +0900

    Fix relationships and add model methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,15 +6,27 @@ class User < ApplicationRecord
 
   has_one_attached :avatar
 
+  has_many :following_relationships, class_name: 'FollowRelationship',
+                                  foreign_key: 'follower_id',
+                                  dependent: :destroy,
+                                  inverse_of: :follower
+  has_many :followings, through: :following_relationships
+
   has_many :follower_relationships, class_name: 'FollowRelationship',
-                                    foreign_key: 'following_id',
-                                    dependent: :destroy,
-                                    inverse_of: :follower
+                                   foreign_key: 'following_id',
+                                   dependent: :destroy,
+                                   inverse_of: :following
   has_many :followers, through: :follower_relationships
 
-  has_many :following_relationships, class_name: 'FollowRelationship',
-                                     foreign_key: 'follower_id',
-                                     dependent: :destroy,
-                                     inverse_of: :following
-  has_many :followings, through: :following_relationships
+  def follow(other_user)
+    following_relationships.create(following_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    following_relationships.find_by(following_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    followings.include?(other_user)
+  end
 end
