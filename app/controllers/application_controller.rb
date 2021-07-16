dfdc778034f3128d0404292848456f06aa44c3e2
Gen Tamura <gen.tamura84@gmail.com>
2021-07-16 18:40:31 +0900

    Update strict params in sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     added_attrs = %i[email postal_code address about password password_confirmation]
+    sign_in_attrs = %i[email password]
 
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
-    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: sign_in_attrs
   end
 end
