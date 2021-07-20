2513064db500f2553e0198b7b07b5e2bdcb4054c
Gen Tamura <gen.tamura84@gmail.com>
2021-07-20 17:07:18 +0900

    Add unit tests in report

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,22 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#editable?' do
+    alice = User.create!(email: 'alice@example.com', password: 'password')
+    bob = User.create!(email: 'bob@example.com', password: 'password')
+
+    report = Report.create!(title: 'report title', content: 'report content', user: alice)
+
+    assert report.editable?(alice)
+    assert_not report.editable?(bob)
+  end
+
+  test '#created_on' do
+    alice = User.create!(email: 'alice@example.com', password: 'password')
+
+    ymd_str = '2021-01-01'
+    report = Report.create!(title: 'report title', content: 'report content', user: alice, created_at: Time.new(ymd_str))
+
+    assert_equal Date.parse(ymd_str), report.created_on
+  end
 end
