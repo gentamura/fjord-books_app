601a944e9d3c9aaa7d0718c42184fc0b6e978e8c
Gen Tamura <gen.tamura84@gmail.com>
2021-07-21 14:20:48 +0900

    Add pathe helper methods for follow

--- app/controllers/follow_relationships_controller.rb
+++ app/controllers/follow_relationships_controller.rb
@@ -4,12 +4,12 @@ class FollowRelationshipsController < ApplicationController
   def create
     user = User.find(params[:following_id])
     current_user.follow(user)
-    redirect_to user
+    redirect_to user_path(user)
   end
 
   def destroy
     user = FollowRelationship.find(params[:id]).following
     current_user.unfollow(user)
-    redirect_to user
+    redirect_to user_path(user)
   end
 end
