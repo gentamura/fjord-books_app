300a8127f9ab6bc648ffe8d65e4202305de052b9
Gen Tamura <gen.tamura84@gmail.com>
2021-07-21 13:34:27 +0900

    Remove paginate scope

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: %i[show followings followers]
 
   def index
-    @users = User.with_attached_avatar.paginate(params[:page])
+    @users = User.with_attached_avatar.order(:id).page(params[:page])
   end
 
   def show; end
